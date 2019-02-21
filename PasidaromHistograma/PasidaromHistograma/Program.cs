cd866137a14f6c17d7101feee304274c80fcc21f
Justas Puodžius <justas.puodzius@gmail.com>
2019-02-21 18:33:29 +0200

    Working Video capture and real time RGB histogram

--- PasidaromHistograma/PasidaromHistograma/Program.cs
+++ PasidaromHistograma/PasidaromHistograma/Program.cs
@@ -16,77 +16,45 @@ namespace AntraPaskaitaHistograma
             Mat img = new Mat();
             Mat video = new Mat();
             Mat gray = new Mat();
-            Mat blue = new Mat();
-            Mat green = new Mat();
-            Mat red = new Mat();
 
-            VideoCapture cap = new VideoCapture(0);
+            Mat[] bgr;
 
-            img = Cv2.ImRead(@"C:\Users\Laptop\Desktop\aa.jpg", LoadMode.Color);
-            gray = img.CvtColor(ColorConversion.RgbToGray);
-
-            Mat[] bgr = Cv2.Split(img);
-            bgr[0].CopyTo(blue);
-            bgr[1].CopyTo(green);
-            bgr[2].CopyTo(red);
+            MatOfByte matr;
+            MatOfByte matg;
+            MatOfByte matb;
 
             float[] histvaluer = new float[256];
             float[] histvalueg = new float[256];
             float[] histvalueb = new float[256];
-            float[] histvalue = new float[256];
 
-            MatOfByte matr = new MatOfByte(red);
-            var idxr = matr.GetIndexer();
-            MatOfByte matg = new MatOfByte(green);
-            var idxg = matg.GetIndexer();
-            MatOfByte matb = new MatOfByte(blue);
-            var idxb = matb.GetIndexer();
-            for (int row = 0; row < gray.Rows; row++)
-            {
-                for (int col = 0; col < gray.Cols;  col++)
-                {
-                    histvaluer[idxr[row, col]]++;
-                    histvalueg[idxg[row, col]]++;
-                    histvalueb[idxb[row, col]]++;
-                }
-            }
+            float maxValueR;
+            float maxValueG;
+            float maxValueB;
 
-            
-
-            float maxValueR = histvaluer.Max();
-            float maxValueG = histvalueg.Max();
-            float maxValueB = histvalueb.Max();
             Mat histarea = new Mat(new Size(256, 110), MatType.CV_8UC3, Scalar.White); //padauginam matrica is 255 nes tada matysim kaip atrod
 
-            for (int i = 0; i<256; i++)
-            {
-                Console.WriteLine(histvalue[i].ToString());
+            Window nicole = new Window("nicole", WindowMode.FreeRatio);
+            Window histoWind = new Window("histo", WindowMode.FreeRatio);
+            VideoCapture cap = new VideoCapture(0);
 
-                //Normalizavimas
-                histvaluer[i] = histvaluer[i] / maxValueR * 100;
-                histvalueg[i] = histvalueg[i] / maxValueG * 100;
-                histvalueb[i] = histvalueb[i] / maxValueB * 100;
-            }
-
-            /*while (true)
+            while (true)
             {
+                histarea = new Mat(new Size(256, 110), MatType.CV_8UC3, Scalar.White);
+
                 cap.Read(img);
-                //show.Image = img;
+                nicole.Image = img;
                 bgr = Cv2.Split(img);
-                bgr[0].CopyTo(blue);
-                bgr[1].CopyTo(green);
-                bgr[2].CopyTo(red);
 
-                matr = new MatOfByte(red);
-                idxr = matr.GetIndexer();
-                matg = new MatOfByte(green);
-                idxg = matg.GetIndexer();
-                matb = new MatOfByte(blue);
-                idxb = matb.GetIndexer();
+                matr = new MatOfByte(bgr[2]);
+                var idxr = matr.GetIndexer();
+                matg = new MatOfByte(bgr[1]);
+                var idxg = matg.GetIndexer();
+                matb = new MatOfByte(bgr[0]);
+                var idxb = matb.GetIndexer();
 
-                for (int row = 0; row < gray.Rows; row++)
+                for (int row = 0; row < img.Rows; row++)
                 {
-                    for (int col = 0; col < gray.Cols; col++)
+                    for (int col = 0; col < img.Cols; col++)
                     {
                         histvaluer[idxr[row, col]]++;
                         histvalueg[idxg[row, col]]++;
@@ -97,21 +65,30 @@ namespace AntraPaskaitaHistograma
                 maxValueR = histvaluer.Max();
                 maxValueG = histvalueg.Max();
                 maxValueB = histvalueb.Max();
-            }*/
 
-            for (int i=0; i<255; i++)
-            {
-                Cv2.Line(histarea, new Point(i, 100 - (int)histvaluer[i]), new Point(i + 1, 100 - (int)histvaluer[i + 1]), Scalar.Red, 1);
-                Cv2.Line(histarea, new Point(i, 100 - (int)histvalueg[i]), new Point(i + 1, 100 - (int)histvalueg[i + 1]), Scalar.Green, 1);
-                Cv2.Line(histarea, new Point(i, 100 - (int)histvalueb[i]), new Point(i + 1, 100 - (int)histvalueb[i + 1]), Scalar.Blue, 1);
-            }
+                for (int i = 0; i < 256; i++)
+                {
+                //Normalizavimas
+                    histvaluer[i] = histvaluer[i] / maxValueR * 100;
+                    histvalueg[i] = histvalueg[i] / maxValueG * 100;
+                    histvalueb[i] = histvalueb[i] / maxValueB * 100;
+                }
 
-            Window nicole = new Window("nicole", WindowMode.FreeRatio);
-            nicole.Image = img;
+                for (int i = 0; i < 255; i++)
+                {
+                    Cv2.Line(histarea, new Point(i, 100 - (int)histvaluer[i]), new Point(i + 1, 100 - (int)histvaluer[i + 1]), Scalar.Red, 1);
+                    Cv2.Line(histarea, new Point(i, 100 - (int)histvalueg[i]), new Point(i + 1, 100 - (int)histvalueg[i + 1]), Scalar.Green, 1);
+                    Cv2.Line(histarea, new Point(i, 100 - (int)histvalueb[i]), new Point(i + 1, 100 - (int)histvalueb[i + 1]), Scalar.Blue, 1);
+                }
 
-            Window histoWind = new Window("histo", WindowMode.FreeRatio);
-            histoWind.Image = histarea;
+                histoWind.Image = histarea;
 
+                GC.Collect(); //Cleaning previous objects to protect from memory leak
+
+                if (Cv2.WaitKey(10) == 'q') { break; }
+        }
+
+            cap.Release();
             Cv2.WaitKey();
 
             nicole.Close();
